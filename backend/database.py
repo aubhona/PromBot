116a44e28d0e16220f8f83f09eea543552e0e7e8
Aub <aubhon.a@gmail.com>
2024-01-24 12:31:10 +0300

    Fix bugs

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.pool import NullPool
 from config import *
 
+
 Base = declarative_base()
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, echo=False, poolclass=NullPool)
 
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
